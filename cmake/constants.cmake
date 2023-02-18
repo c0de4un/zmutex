5ffb1831b5dfb0da4939d40f386d18f1bab1a69b
c0de4un <code4un@yandex.ru>
2023-02-18 14:25:18 +0400

    Update CMakeLists

--- cmake/constants.cmake
+++ cmake/constants.cmake
@@ -7,6 +7,14 @@
 set( PROJECT_CMAKE_VERSION 3.22.1 )
 set( APP_VERSION 0.1.0 )
 
+if ( NOT DEFINED ZMUTEX_C_STANDARD )
+    set( ZMUTEX_C_STANDARD 99 )
+endif ( NOT DEFINED ZMUTEX_C_STANDARD )
+
+if ( NOT DEFINED ZMUTEX_CXX_STANDARD )
+    set( ZMUTEX_CXX_STANDARD 14 )
+endif ( NOT DEFINED ZMUTEX_CXX_STANDARD )
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # DIRS
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
