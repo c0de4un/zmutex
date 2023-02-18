b0688d5706718812b3652d2dc76537c6e0656681
c0de4un <code4un@yandex.ru>
2023-02-18 12:15:18 +0400

    Hello World!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zmutex
-Lightweight Mutex Library with Shared support. Designed as cross-platform alternative to C++ 17 shared_mutex with more flexibility. Perfect solution for simple mobile games, when advanced STL 17 features are not always available
+Lightweight Mutex Library with Shared support.
+Designed as cross-platform alternative to C++ 17 shared_mutex with more flexibility.
+Perfect solution for simple mobile games, when advanced STL 17 features are not always available
+
+[![GitHub license](https://img.shields.io/github/license/c0de4un/zmutex)](https://github.com/c0de4un/zmutex/blob/main/LICENSE)
+[![GitHub stars](https://img.shields.io/github/stars/c0de4un/zmutex)](https://github.com/c0de4un/zmutex/stargazers)
---
+++ cmake/globals.cmake
@@ -0,0 +1,22 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+if ( NOT DEFINED ZMUTEX_DIR )
+    set( ZMUTEX_DIR ${CMAKE_CURRENT_SOURCE_DIR} )
+endif ( NOT DEFINED ZMUTEX_DIR )
+
+if ( NOT DEFINED ZMUTEX_SRC_DIR )
+    set( ZMUTEX_SRC_DIR "${ZMUTEX_DIR}/src" )
+endif ( NOT DEFINED ZMUTEX_SRC_DIR )
+
+if ( NOT DEFINED ZMUTEX_TESTS_DIR )
+    set( ZMUTEX_TESTS_DIR "${ZMUTEX_DIR}/tests" )
+endif ( NOT DEFINED ZMUTEX_TESTS_DIR )
+
+if ( NOT DEFINED ZMUTEX_OUTPUT_DIR )
+    set( ZMUTEX_OUTPUT_DIR "${ZMUTEX_DIR}/bin" )
+endif ( NOT DEFINED ZMUTEX_OUTPUT_DIR )
+
+set( ZMUTEX_HEADERS "" )
+set( ZMUTEX_SOURCES "" )
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
